1ed66cfab50c4cc2280ccf240c70b82bf54598a2
Paul Debliccker <paul.deblicker@gmail.com>
2017-07-02 19:11:33 +0200

    Rename functions and vars

--- add_feature_to_other_features.py
+++ add_feature_to_other_features.py
@@ -2,30 +2,30 @@ import json
 import os
 import sys
 
-def getFeature(filePath, featureId):
-    data = getGeojsonData(os.path.join(filePath))
+def get_feature(file_path, feature_id):
+    data = get_geojson_data(os.path.join(file_path))
 
-    return [f for f in data['features'] if f['id'] == featureId][0]
+    return [f for f in data['features'] if f['id'] == feature_id][0]
 
-def addFeature(filePath, feature):
-    data = getGeojsonData(os.path.join(filePath))
+def add_feature(file_path, feature):
+    data = get_geojson_data(os.path.join(file_path))
 
     data['features'].append(feature)
     return data
 
-def getGeojsonData(path):
+def get_geojson_data(path):
     with open(path, encoding='utf-8') as f:
         data = json.load(f)
         f.close();
 
     return data;
 
-def writeGeojson(path, data):
+def write_geojson(path, data):
     with open(path, 'w') as f:
         json.dump(data, f)
         f.close
 
 if __name__ == '__main__':
-    feature = getFeature(sys.argv[1], int(sys.argv[2]))
-    newProvinces = addFeature(sys.argv[3], feature)
-    writeGeojson(os.path.split(sys.argv[3])[1], newProvinces)
+    feature = get_feature(sys.argv[1], int(sys.argv[2]))
+    new_provinces = add_feature(sys.argv[3], feature)
+    write_geojson(os.path.split(sys.argv[3])[1], new_provinces)
